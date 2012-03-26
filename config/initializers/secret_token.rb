ff7336f90dcb5ccadd431483ba82fd9b880df008
Fedor Nikolaev <fsqcds@gmail.com>
2012-03-26 23:25:38 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cooluncool::Application.config.secret_token = 'f92c0ca25ca7a440fb6bd696126d443857b3959d5e4b2fc7c4df0d0c87fd0167c202dec5e4b9e053b46d505f7df61c65b2c781c18e9c3fb3f02aa2cf317ce6c5'
